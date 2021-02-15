bdaf9f02a050d2c4923935f692b53013ae8e977a
Dynna <ciorbadina9@gmail.com>
2021-02-15 15:58:47 +0200

    add supervisor configuration

---
+++ PTR/rtp_lab/lib/rtp_lab.ex
@@ -0,0 +1,18 @@
+defmodule RtpLab do
+  @moduledoc """
+  Documentation for `RtpLab`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RtpLab.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ PTR/rtp_lab/test/rtp_lab_test.exs
@@ -0,0 +1,8 @@
+defmodule RtpLabTest do
+  use ExUnit.Case
+  doctest RtpLab
+
+  test "greets the world" do
+    assert RtpLab.hello() == :world
+  end
+end
