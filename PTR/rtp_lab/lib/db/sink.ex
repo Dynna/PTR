670591acf0731590b99c92d84026eb2d7312c585
Dynna <ciorbadina9@gmail.com>
2021-03-23 17:27:02 +0200

    add users in separate collection

--- PTR/rtp_lab/lib/db/sink.ex
+++ PTR/rtp_lab/lib/db/sink.ex
@@ -12,33 +12,22 @@ defmodule Sink do
   end
 
   @impl true
-  def handle_cast({:message, message}, _smth) do
-    MyIO.my_inspect("ADDING ACTUAL TWEET")
-  #  Backpressure.insert(message)
-
-   {:noreply, %{}}
-  end
-
-  @impl true
   def handle_cast({:user, user}, _smth) do
     MyIO.my_inspect("ADDING USERS")
-    Backpressure.load(%{user: user}, "users")
-
+    Backpressure.insert_users(user)
     {:noreply, %{}}
   end
 
   @impl true
-  def handle_cast({:ratio, ratio}, _smth) do
-    MyIO.my_inspect("ADDING ENGAGEMENT RATIO")
-   # Backpressure.load(%{ratio: ratio}, "tweets")
-    Backpressure.insert(ratio)
+  def handle_cast({:tweet_ratio, tweet_ratio}, _smth) do
+    MyIO.my_inspect("ADDING ENGAGEMENT RATIO + ACTUAL TWEET")
+    Backpressure.insert(tweet_ratio)
     {:noreply, %{}}
   end
 
   @impl true
   def handle_cast({:sentiment_score, sentiment_score}, _smth) do
     MyIO.my_inspect("ADDING SENTIMENT SCORE")
-    #Backpressure.load(%{sentiment_score: sentiment_score}, "tweets")
     Backpressure.insert(sentiment_score)
     {:noreply, %{}}
   end
